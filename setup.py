21511aaec1235972d0961fa43b85441fa78c4dc2
MariaIsabelSoto24 <maria.sotoio@est.iudigital.edu.co>
2025-03-03 04:41:22 +0000

    carga inicial

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Maria Soto",
+    author_email="maria.sotoio@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
